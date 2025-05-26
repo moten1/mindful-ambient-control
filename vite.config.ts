1a99c95c0eff548fc9c46aaa3df7ad1ff3bada2b
moten1 <moten1@gmail.com>
2025-05-26 12:40:35 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -23,5 +22,8 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
   }
 }));
